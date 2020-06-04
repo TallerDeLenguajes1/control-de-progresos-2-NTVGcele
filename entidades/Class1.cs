c930ab308bc69bf4fcfdae24b722affab81609e3
celeste <mesientofuerte22@gmail.com>
2020-06-04 17:44:48 -0300

    Carga de personajes a la lista

--- entidades/Class1.cs
+++ entidades/Class1.cs
@@ -12,25 +12,30 @@ namespace entidades
 
    public class Personaje
    {
-            private Caracteristicas caract;
-            private Datos dat;
+        private Caracteristicas caract;
+        private Datos dat;
+        private int id;
 
         public Personaje()
         {
         }
 
-        public Personaje(string nombre, string apodo )
+        public Personaje(string nombre, string apodo, int _Id )
         {
             Caract = new Caracteristicas();
             Dat = new Datos(nombre, apodo);
+            Id = _Id;
         }
 
         public Caracteristicas Caract { get => caract; set => caract = value; }
         public Datos Dat { get => dat; set => dat = value; }
+        public int Id { get => id; set => id = value; }
+
         public void MostrarPersonajes()
         {
-            Console.Write("\nDatos: ");
-            Console.Write("\n\nTipo de jugador: " + Dat.TipoJugador);
+            Console.Write("\n_______________________________");
+            Console.Write("\n\nDatos del Jugador " + Id + ": ");
+            Console.Write("\nTipo de jugador: " + Dat.TipoJugador);
             Console.Write("\nNombre: " + Dat.Nombre);
             Console.Write("\nApodo: : " + Dat.Apodo);
             Console.Write("\nFecha de Nacimiento: " + Dat.FechaNacimiento);
@@ -38,11 +43,11 @@ namespace entidades
             Console.Write("\nSalud: " + Dat.Salud);
 
             Console.Write("\n\nCaracteristicas: ");
-            Console.Write("\n\nVelocidad" + Caract.Velocidad);
-            Console.Write("\nDestreza" + Caract.Destreza);
-            Console.Write("\nFuerza" + Caract.Fuerza);
-            Console.Write("\nNivel" + Caract.Nivel);
-            Console.Write("\nArmadura" + Caract.Armadura);
+            Console.Write("\nVelocidad: " + Caract.Velocidad);
+            Console.Write("\nDestreza: " + Caract.Destreza);
+            Console.Write("\nFuerza: " + Caract.Fuerza);
+            Console.Write("\nNivel: " + Caract.Nivel);
+            Console.Write("\nArmadura: " + Caract.Armadura);
 
             
     }
@@ -88,7 +93,7 @@ namespace entidades
             FechaNacimiento = new DateTime(rnd.Next(1800, 2020), rnd.Next(1, 12), rnd.Next(1, 30));
             Edad = rnd.Next(1, 300);
             Salud = rnd.Next(0, 100);
-            TipoJugador = (Tipo)rnd.Next(0, 2);
+            TipoJugador = (Tipo)rnd.Next(0, 3);
         }
 
         public Datos()
--- trabajoEnClases/Program.cs
+++ trabajoEnClases/Program.cs
@@ -1,14 +1,25 @@
 ﻿using entidades;
 using System;
+using System.Collections.Generic;
 
 namespace trabajoEnClases
 {
     class Program
     {
+        static int cantidad = 2;
         static void Main(string[] args)
         {
-            Personaje jugador = new Personaje("Celeste", "NTVG");
-            jugador.MostrarPersonajes();
+            Personaje jugador = new Personaje();
+            List<Personaje> ListaDeJugadores = new List<Personaje>();
+            string[] Nombres = { "Celeste", "Gonzalo", "Fabio", "Alex", "Fede" };
+            string[] Apodos = { "Ntvg", "Sotaski", "Soto", "Juampy", "kiko" };
+            for(int i = 0; i < cantidad; i++)
+            {
+                jugador = new Personaje(Nombres[new Random().Next(0, 4)], Apodos[new Random().Next(0, 4)], i+1);
+                ListaDeJugadores.Add(jugador);
+                jugador.MostrarPersonajes();
+            }
+            
         }
     }
 }
